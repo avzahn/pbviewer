cb985b0cf04033edb318ec522420ec7812d6aeda
Alex Zahn <avzahn@ucsd.edu>
2015-10-19 16:48:26 -0700

    .

--- pbviewer.py
+++ pbviewer.py
@@ -20,9 +20,12 @@ class ControlClient(object):
 		self.socket.close()
 		
 	def __call__(self,msg):
+		"""
+		Dispatch a message to GCP and return the response contents
+		"""
 		try:
 			self.socket.send(self.pack(msg))
-			return self.socket.recv(self.buffsize)
+			return self.unpack(self.socket.recv(self.buffsize))[1]
 		except:
 			print>>sys.stderr, "Connection failed; attempting reconnect"
 			self.socket.close()
@@ -30,10 +33,40 @@ class ControlClient(object):
 			self.socket.connect((self.host,self.port))
 			
 	def pack(self,msg):
+		"""
+		Form a GCP command packet with msg as the contents
+		
+		The format appears to be a 12 byte header followed by msg.
+		The first two header bytes seem to be zero, followed by two
+		bytes for the message length in bytes plus twelve, followed
+		by six bytes of zeros, followed by two bytes the message
+		length
+		"""
 		l=len(msg)
 		return struct.pack('!6H%ic'%l,0,l+12,0,0,0,l,*list(msg))
-
-with ControlClient('localhost',45481) as ctl:
-	while True:
-		print ctl(raw_input("GCP> "))
+		
+	def unpack(self,rsp):
+		"""
+		Return a (header, contents) tuple from GCP responses
+		
+		The first 12 bytes of a response appear to be some sort
+		of header that behaves similarly to the header expected
+		from command messages. I haven't actually figured out
+		how this response header works, but there seems to be
+		little harm in ignoring it
+		"""
+		return rsp[0:11],rsp[11:]
+		
+if __name__ == "__main__":
+	
+	if len(sys.argv) = 1:
+		host = 'localhost'
+		port = 45481
+	else:
+		host = sys.argv[1]
+		port = sys.argv[2]
+	
+	with ControlClient(host,port) as ctl:
+		while True:
+			print ctl(raw_input("GCP> "))
 
